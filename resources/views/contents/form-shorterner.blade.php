4f7960268cf3b4b0c384a67a58099df0b5af57a3
Arief Fahrizon <arieffahrizon@gmail.com>
2019-09-14 00:46:28 +0700

    finishing

--- resources/views/contents/form-shorterner.blade.php
+++ resources/views/contents/form-shorterner.blade.php
@@ -10,6 +10,9 @@
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
+		<!-- <link rel="stylesheet" href="{{ url('public/bootstrap/dist/css/bootstrap.min.css') }}"> -->
+		<!-- <link rel="stylesheet" href="{{ url('public/bootstrap/dist/css/bootstrap-theme.min.css') }}"> -->
+
 		<link rel="stylesheet" href="{{ url('public/css/sweetalert.css') }}">
 
 		<style>
@@ -29,13 +32,22 @@
 				border-radius:10px;
 				box-shadow: 0px 0px 5px 1px #555;
 			}
+			hr {
+			  height: 1.5px;
+			}
+			.hr-success{
+			  background-image: -webkit-linear-gradient(left, rgba(15,157,88,.8), rgba(15, 157, 88,.6), rgba(0,0,0,0));
+			}
+			#for-result{
+				display: none;
+			}
 		</style>
 	</head>
 	<body>
 		<div class="container container-kecil">
 			<div class="row">
 				<div class="col-md-12">
-					<form method='post'>
+					<!-- <form method='post'> -->
 						<div class="row">
 							<div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
 								<h3>Buat URL Anda menjadi singkat, hanya sekali klik</h3>
@@ -49,7 +61,14 @@
 								</button>
 							</div>
 						</div>
-					</form>
+						<hr class="hr-success" id="for-result">
+						<div class="row" id="for-result">
+							<div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
+								<label>Link baru Anda : </label><br>
+								<a href='#' id='new-link'></a>
+							</div>
+						</div>
+					<!-- </form> -->
 				</div>
 			</div>
 		</div>
@@ -57,7 +76,63 @@
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
+		<!-- <script src="{{ url('public/bootstrap/dist/js/bootstrap.min.js') }}"></script> -->
+
+		<script src="{{ url('public/jquery/jquery.js') }}"></script>
+		<script src="{{ url('public/jquery/jquery.min.js') }}"></script>
+
 		<script src="{{ url('public/js/sweetalert.min.js') }}"></script>
 		@include('sweet::alert')
+
+		<script type="text/javascript">
+			$(document).ready(function(){
+        		$('#btn-generate').click(function(){
+        			var previous_link = $('#link-address').val();
+
+        			$.ajax({
+		                url : '{{ url("api/generating/new-link") }}',
+		                type:'POST',
+		                data: JSON.stringify({ 
+		                    "previous_link" : previous_link,
+		                }),
+		                contentType: "application/json",
+		                dataType: 'json',
+		                success: function(output_nya){
+		                	if(output_nya.code == "200"){
+		                		var new_link = output_nya.new_link;
+
+		                		$('#for-result').css('display','block');
+
+		                		$('#new-link').html(new_link);
+
+		                        swal({
+		                            title: "Yeay..",
+		                            text: output_nya.message,
+		                            showConfirmButton: false,
+		                            timer: 2500,
+		                            type: "success",
+		                        });
+		                    }else{
+		                    	$('#for-result').css('display','none');
+
+		                    	$('#new-link').html("");
+
+		                        swal({
+		                            title: "Oops..",
+		                            text: output_nya.message,
+		                            showConfirmButton: false,
+		                            timer: 2500,
+		                            type: "error",
+		                        });    
+		                    }
+		                },
+		                error: function(xhr, status, error) {
+		                  var err = eval("(" + xhr.responseText + ")");
+		                  alert(err.Message);
+		                }
+		            });
+        		});
+        	});
+		</script>
 	</body>
 </html>
